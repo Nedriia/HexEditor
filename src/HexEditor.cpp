e8da87b3e50c71d7d06b80223b870233427f3730
Nedriia <arthur.favre@hotmail.com>
2026-07-17 11:20:14 +0200

    add files to fix builds

---
+++ src/HexEditor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arthu on 17/07/2026.
+//
+
+#include "HexEditor.h"
---
+++ src/HexEditor.h
@@ -0,0 +1,12 @@
+//
+// Created by arthu on 17/07/2026.
+//
+
+#ifndef HEXEDITOR_HEXEDITOR_H
+#define HEXEDITOR_HEXEDITOR_H
+
+
+class HexEditor {};
+
+
+#endif //HEXEDITOR_HEXEDITOR_H
